f2a0fda6d6515b85148114809bfb199c9f3d81fd
WiemeJarne <94701784+WiemeJarne@users.noreply.github.com>
2024-09-15 16:30:27 +0200

    moved all css code to 1 file, removed unused files and added Tea for the Queen project to project section

---
+++ scripts/imagesCycle.js
@@ -0,0 +1,64 @@
+const grappleItOutImages= 
+[
+    "images/projects/grappleitout/GrappleItOut.png",
+    "images/projects/grappleitout/GrappleItOut01.png",
+    "images/projects/grappleitout/GrappleItOut02.png",
+    "images/projects/grappleitout/GrappleItOut03.png",
+    "images/projects/grappleitout/GrappleItOut04.png",
+    "images/projects/grappleitout/GrappleItOut05.png"
+];
+
+const VRSHoppingImages=
+[
+    "images/projects/VRSHopping/VRShopping01.png",
+    "images/projects/VRSHopping/VRShopping02.png",
+    "images/projects/VRSHopping/VRShopping03.png",
+    "images/projects/VRSHopping/VRShopping04.png",
+    "images/projects/VRSHopping/VRShopping05.png",
+    "images/projects/VRSHopping/VRShopping06.png",
+    "images/projects/VRSHopping/VRShopping07.png",
+    "images/projects/VRSHopping/VRShopping08.png",
+    "images/projects/VRSHopping/VRShopping09.png",
+    "images/projects/VRSHopping/VRShopping10.png",
+    "images/projects/VRSHopping/VRShopping11.png"
+];
+
+const TeaForTheQueenImages=
+[
+    "images/projects/TeaForTheQueen/TeaForTheQueen.png",
+    "images/projects/TeaForTheQueen/TeaForTheQueen01.png",
+    "images/projects/TeaForTheQueen/TeaForTheQueen02.png",
+    "images/projects/TeaForTheQueen/TeaForTheQueen03.png",
+    "images/projects/TeaForTheQueen/TeaForTheQueen04.png",
+    "images/projects/TeaForTheQueen/TeaForTheQueen05.png",
+    "images/projects/TeaForTheQueen/TeaForTheQueen06.png",
+    "images/projects/TeaForTheQueen/TeaForTheQueen07.png",
+];
+
+window.onload = function()
+{
+    cycleImage(grappleItOutImages, "GrappleItOutImage");
+    cycleImage(VRSHoppingImages, "VRShoppingImage");
+    cycleImage(TeaForTheQueenImages, "TeaForTheQueenImage");
+}
+
+function cycleImage(images, elementId)
+{
+    let currentImageIndex = 0;
+
+    function updateImage()
+    {
+        ++currentImageIndex;
+
+        if(currentImageIndex >= images.length)
+            currentImageIndex = 0;
+
+        image = document.getElementById(elementId);
+
+        image.src=images[currentImageIndex];
+
+        setTimeout(updateImage, 3000);
+    }
+    
+    updateImage();
+}
