6cf45ef2a6b96e636b2008c9a14a0b08e1e6371f
WiemeJarne <94701784+WiemeJarne@users.noreply.github.com>
2024-09-18 15:43:00 +0200

    added next projects: AI pathfinding, CheapShark api, 2D game engine, Burger Time, Dual rasterizer and Software ray tracer

--- scripts/imagesCycle.js
+++ scripts/imagesCycle.js
@@ -58,6 +58,53 @@ const AISteeringBehaviorsImages=
     "images/projects/AISteeringBehaviors/AISteeringBehaviors05.png"
 ];
 
+const AIPathfindingImages=
+[
+    "images/projects/AIPathfinding/AIPathfinding01.png",
+    "images/projects/AIPathfinding/AIPathfinding02.png",
+    "images/projects/AIPathfinding/AIPathfinding03.png",
+    "images/projects/AIPathfinding/AIPathfinding04.png",
+    "images/projects/AIPathfinding/AIPathfinding05.png",
+    "images/projects/AIPathfinding/AIPathfinding06.png",
+    "images/projects/AIPathfinding/AIPathfinding07.png"
+];
+
+const CheapSharkAPI_Images=
+[
+    "images/projects/CheapSharkAPI/CheapSharkAPI01.png",
+    "images/projects/CheapSharkAPI/CheapSharkAPI02.png",
+    "images/projects/CheapSharkAPI/CheapSharkAPI03.png",
+    "images/projects/CheapSharkAPI/CheapSharkAPI04.png"
+];
+
+const BurgerTimeImages=
+[
+    "images/projects/BurgerTime/BurgerTime01.png",
+    "images/projects/BurgerTime/BurgerTime02.png",
+    "images/projects/BurgerTime/BurgerTime03.png",
+    "images/projects/BurgerTime/BurgerTime04.png",
+    "images/projects/BurgerTime/BurgerTime05.png",
+    "images/projects/BurgerTime/BurgerTime06.png"
+];
+
+const DualRasterizerImages=
+[
+    "images/projects/DualRasterizer/DualRasterizer01.png",
+    "images/projects/DualRasterizer/DualRasterizer02.png",
+    "images/projects/DualRasterizer/DualRasterizer03.png",
+    "images/projects/DualRasterizer/DualRasterizer04.png",
+    "images/projects/DualRasterizer/DualRasterizer05.png",
+    "images/projects/DualRasterizer/DualRasterizer06.png",
+    "images/projects/DualRasterizer/DualRasterizer07.png",
+    "images/projects/DualRasterizer/DualRasterizer08.png"
+];
+
+const SoftwareRayTracerImages=
+[
+    "images/projects/SoftwareRayTracer/SoftwareRayTracer01.png",
+    "images/projects/SoftwareRayTracer/SoftwareRayTracer02.png"
+];
+
 window.onload = function()
 {
     cycleImage(grappleItOutImages, "GrappleItOutImage");
@@ -66,6 +113,11 @@ window.onload = function()
     cycleImage(ZombieAIGameImages, "ZombieAIGameImage");
     cycleImage(FlowFieldPathfindingImages, "FlowFieldPathfindingImage");
     cycleImage(AISteeringBehaviorsImages, "AISteeringBehaviorsImage");
+    cycleImage(AIPathfindingImages, "AIPathfindingImage");
+    cycleImage(CheapSharkAPI_Images, "CheapSharkAPI_Image");
+    cycleImage(BurgerTimeImages, "BurgerTimeImage");
+    cycleImage(DualRasterizerImages, "DualRasterizerImage");
+    cycleImage(SoftwareRayTracerImages, "SoftwareRayTracerImage");
 }
 
 function cycleImage(images, elementId)
